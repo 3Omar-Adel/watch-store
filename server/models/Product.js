ed87ffe14e097663399cf0fb2717020790d9c614
Omar Adel <oadel425@gmail.com>
2026-07-11 17:50:04 +0300

    created Product model

---
+++ server/models/Product.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose")
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    brand: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    images: [
+        {
+            type: String,
+        },
+    ],
+    stock: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    featured: {
+        type: Boolean,
+        default: false,
+    },
+    },
+        {
+            timestamps: true,
+        }
+)
+
+module.exports = mongoose.model("Product", productSchema)
